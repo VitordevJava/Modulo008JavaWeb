7ec6b317e7538178fb86391d2970dc22b2b6e880
VitordevJava <vitordev.java@gmail.com>
2024-08-03 20:48:51 -0300

    Novo acrescimo no codigo, OPERADORES TERNARIOS, USADOS PARA MICRO VERIFICAÇÕES.

--- Modulo008JavaWeb/src/exerciciosMod008/OperadoresLogicos.java
+++ Modulo008JavaWeb/src/exerciciosMod008/OperadoresLogicos.java
@@ -4,11 +4,12 @@ public class OperadoresLogicos {
 	public static void main(String[] args) {
 
 		int N1 = 80;
-		int N2 = 10;
+		int N2 = 90;
 
 		double N3 = ( N1 + N2)/ 2;
 		String name = ("Vitor");
-
+    
+		/* OPERAÇÕES LOGICAS COM IF e ELSE*/
 		if (N3 >= 70 && name.equals("Vitor")) {
 			System.out.println("Parabéns você foi aprovado!!");
 		} else if (N3 < 70) {
@@ -17,6 +18,13 @@ public class OperadoresLogicos {
 		} else {
 			System.out.println("Uma pena, não encontramos o aluno, tente verificar se está com o Nome correto");
 		}
+		
+		/*OPERADORES TERNARIOS São para micro validações*/
+		
+		String SaidaResultado;
+		SaidaResultado = N3 >= 70 ? "Aluno aprovado" : "Aluno reprovado";
+		
+		System.out.println(SaidaResultado);
 
 	}
 }
