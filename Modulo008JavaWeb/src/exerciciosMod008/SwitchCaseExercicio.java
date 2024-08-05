9f7a40180b5087ae16bfef10e0db76597833ee8d
VitordevJava <vitordev.java@gmail.com>
2024-08-04 23:44:05 -0300

    Adicionando novo exercicio de Switch e case ao repositorio.

---
+++ Modulo008JavaWeb/src/exerciciosMod008/SwitchCaseExercicio.java
@@ -0,0 +1,39 @@
+package exerciciosMod008;
+
+public class SwitchCaseExercicio {
+	public static void main(String[] args) {
+		int dia1 = 1;
+		int dia2 = 2;
+		int dia3 = 3;
+		int dia4 = 4;
+		int dia5 = 5;
+
+		switch (dia1) {
+		case 1:
+			System.out.println("Segunda");
+			break;
+		}
+		switch (dia2) {
+		case 2:
+			System.out.println("Terça");
+			break;
+		}
+		switch (dia3) {
+		case 3:
+			System.out.println("Quarta");
+			break;
+		}
+		switch (dia4) {
+		case 4:
+			System.out.println("Quinta");
+			break;
+		}
+		switch (dia5) {
+		case 5:
+			System.out.println("Sexta");
+			break; 
+		}
+
+	}
+
+}
