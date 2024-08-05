76098906c8e02fe2882e4aafac908ae9845910c4
VitordevJava <vitordev.java@gmail.com>
2024-08-05 00:03:00 -0300

    Adicionando ao GitHub novo exercico de operadores relacionais.

---
+++ Modulo008JavaWeb/src/exerciciosMod008/OperadoresRelacionais.java
@@ -0,0 +1,18 @@
+package exerciciosMod008;
+
+public class OperadoresRelacionais {
+	public static void main(String[] args) {
+		int nota1= 93;
+		int nota2= 90;
+	
+		/* Para serem comparados se são iguais serão usados dois sinais de igualdade : == /
+		 *  já se são diferentes será usado a exclamação de junto do sinal da igualdade : != /
+		 */
+		
+		if (nota1 != nota2) {
+			System.out.println("As notas são diferentes");
+		}else {
+			System.out.println("As notas são iguais");			
+		}
+	}
+}
