4642b494a563768b28849f11de5e316689bdd47b
VitordevJava <vitordev.java@gmail.com>
2024-08-06 20:27:29 -0300

    Novo exercicio de FOR e Breack.

---
+++ Modulo008JavaWeb/src/exerciciosMod008/ExercicioForBreack.java
@@ -0,0 +1,17 @@
+package exerciciosMod008;
+
+public class ExercicioForBreack {
+	public static void main (String[] args) {
+		
+		for(int numero = 0; numero >= 10; ++numero) {
+			++numero;
+			if(numero == 7) {
+				System.out.println("Opa encontrei seu numero da sorte");
+				System.out.println("Parando de executar");
+			}
+		}
+			
+			
+	}
+
+}
