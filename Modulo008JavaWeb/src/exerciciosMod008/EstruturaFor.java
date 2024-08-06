7987e515146eeee772c3964704a01e52014a67b5
VitordevJava <vitordev.java@gmail.com>
2024-08-05 23:22:33 -0300

    Exercicio da estrutura de repetição FOR.

---
+++ Modulo008JavaWeb/src/exerciciosMod008/EstruturaFor.java
@@ -0,0 +1,15 @@
+package exerciciosMod008;
+
+public class EstruturaFor {
+	public static void main(String[] args) {
+		
+		for(int numero = 50; numero >= 0; numero--) {
+		System.out.println("Seu numero é :" +numero);	
+		}
+		
+	}
+	
+
+}
+
+
