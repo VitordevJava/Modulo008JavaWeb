b52353290bd27a206e789faa8157700c1b331d72
VitordevJava <vitordev.java@gmail.com>
2024-08-05 22:54:37 -0300

    Novo exercicio de While e DO While.

---
+++ Modulo008JavaWeb/src/exerciciosMod008/WhileDoWhile.java
@@ -0,0 +1,22 @@
+package exerciciosMod008;
+
+public class WhileDoWhile {
+	public static void main(String[] args) {
+
+/*	int numero = 0;
+
+		while (numero <= 10) {
+			System.out.println("A contagem ficou assim :" + numero);
+			numero++;
+		} */
+
+		int numero2 = 0;
+
+		do { /*EXECUTA DEPOIS VERIFICA*/
+			System.out.println("O NUMERO ATUAL É :" + numero2);
+			numero2++;
+		} while (numero2 <= 20);
+		
+
+	}
+}
