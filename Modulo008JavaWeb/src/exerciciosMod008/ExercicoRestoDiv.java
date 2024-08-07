4c10c20fa450776bf75996c8e8ed61e248a16676
VitordevJava <vitordev.java@gmail.com>
2024-08-06 21:40:23 -0300

    NOVO EXERCICO DE RESTO DE DIVISÃO.

---
+++ Modulo008JavaWeb/src/exerciciosMod008/ExercicoRestoDiv.java
@@ -0,0 +1,14 @@
+package exerciciosMod008;
+
+public class ExercicoRestoDiv {
+	public static void main(String[] args){
+		
+		double carros = 9;
+		double pessoas = 2;
+		
+		double resto = carros % pessoas;
+		System.out.println("O numero de carros que sobrou apos a divisão é : " + resto + " carros");
+		
+	}
+	
+}
